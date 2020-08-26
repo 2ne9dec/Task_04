92f5c0ac16b90fea1364d8c15732296197165ab7
2ne9dec <o-maestpo@yandex.ru>
2020-08-26 21:23:00 +1200

    task_04_1

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-let toDoList = ['Сходить в магазин', 'Уборка', 'Готовка'];
+let toDoList = ['Task1', 'Task2', 'Task3'];
 
 const input = document.querySelector('.input')
 const add = document.querySelector('#add');
@@ -6,16 +6,22 @@ const remove = document.querySelector('#remove');
 const ul = document.querySelector('#ul');
 
 for (let i = 0; i < toDoList.length; i++) {
+    
     const li = document.createElement('li');
-    li.append([toDoList[i]]);
+    li.append(toDoList[i]);
     ul.append(li);
 };
+console.dir(ul)
 
-let resultAdd = () => {
+const resultAdd = () => {
     const li = document.createElement('li');
     li.textContent = input.value;
     ul.append(li);
-    toDoList.push(li.textContent)
+    toDoList.push(li.textContent);
 };
-
 add.addEventListener('click', resultAdd);
+
+
+for (let list of toDoList) {
+    
+}
