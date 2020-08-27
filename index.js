3d7b20ea6a3f1ffb48f8d0aad72a8ca2c9f65b00
2ne9dec <o-maestpo@yandex.ru>
2020-08-27 21:31:09 +1200

    task_04

--- index.js
+++ index.js
@@ -1,30 +1,28 @@
-let toDoList = ['Task1', 'Task2', 'Task3'];
-const input = document.querySelector('.input')
+const toDoList = ['Task1', 'Task2', 'Task3'];
+const input = document.querySelector('.input');
 const add = document.querySelector('#add');
 const ul = document.querySelector('#ul');
 
-for (let i = 0; i < toDoList.length; i++) {
-    const li = document.createElement('li');
-    li.append(toDoList[i]);
-    ul.append(li);
-    const button = document.createElement('button');
-    button.textContent = 'Delete'
-    li.append(button);
+const render = () => {
+    for (let i = 0; i < toDoList.length; i++) {
+        const li = document.createElement('li');
+        li.append(toDoList[i]);
+        ul.append(li);
+        const button = document.createElement('button');
+        button.addEventListener('click', () => {
+            toDoList.splice(i, 1);
+            ul.innerHTML = null;
+            render();
+        });
+        button.textContent = 'Delete';
+        li.append(button);
+    };
 };
+render();
 
-const addTask = () => {
-    const li = document.createElement('li');
-    li.textContent = input.value;
-    ul.append(li);
-    toDoList.push(li.textContent);
-    const button = document.createElement('button');
-    button.innerHTML = 'Delete';
-    li.append(button);
-};
-    add.addEventListener('click', addTask);
-
-    const button = document.querySelectorAll('button')
-    console.log(button);
-    for (let list of toDoList) {
-        
-    }
+    add.addEventListener('click', () => {
+        toDoList.unshift(input.value);
+        ul.innerHTML = null;
+        render();
+        input.value = null;
+    });
