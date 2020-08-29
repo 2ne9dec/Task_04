efc04d7fe0be6c27031e1ea7a77d11197e83cdc4
2ne9dec <o-maestpo@yandex.ru>
2020-08-30 01:32:49 +1200

    task_04

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const toDoList = ['Task1', 'Task2', 'Task3'];
+let toDoList = ['Task1', 'Task2', 'Task3'];
 const input = document.querySelector('.input');
 const add = document.querySelector('#add');
 const ul = document.querySelector('#ul');
@@ -6,23 +6,28 @@ const ul = document.querySelector('#ul');
 const render = () => {
     for (let i = 0; i < toDoList.length; i++) {
         const li = document.createElement('li');
-        li.append(toDoList[i]);
-        ul.append(li);
+        const div = document.createElement('div');
+        div.innerHTML = toDoList[i];
+        li.append(div);
         const button = document.createElement('button');
+        button.innerHTML = 'Delete';
+        li.append(button);
+        ul.append(li);
         button.addEventListener('click', () => {
             toDoList.splice(i, 1);
             ul.innerHTML = null;
+            localStorage.setItem('newArray', JSON.stringify(toDoList));
             render();
         });
-        button.textContent = 'Delete';
-        li.append(button);
     };
+    JSON.parse(localStorage.getItem('newArray'));
 };
 render();
 
     add.addEventListener('click', () => {
-        toDoList.unshift(input.value);
+        toDoList.push(input.value);
         ul.innerHTML = null;
+        localStorage.setItem('newArray', JSON.stringify(toDoList));
         render();
         input.value = null;
     });
