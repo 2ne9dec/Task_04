c1235178ab452d10a68b9d65ad906d227867c289
2ne9dec <o-maestpo@yandex.ru>
2020-08-31 11:45:42 +1200

    task_04

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-let toDoList = [];
+const toDoList = [];
 const input = document.querySelector('.input');
 const add = document.querySelector('#add');
 const ul = document.querySelector('#ul');
@@ -7,27 +7,37 @@ const render = () => {
     for (let i = 0; i < toDoList.length; i++) {
         const li = document.createElement('li');
         const div = document.createElement('div');
+        const button = document.createElement('button');
         div.innerHTML = toDoList[i];
         li.append(div);
-        const button = document.createElement('button');
         button.innerHTML = 'Delete';
         li.append(button);
         ul.append(li);
+
         button.addEventListener('click', () => {
             toDoList.splice(i, 1);
             ul.innerHTML = null;
-            localStorage.setItem('newArray', JSON.stringify(toDoList));
+            localStorage.setItem('list', JSON.stringify(toDoList));
             render();
         });
     };
-    JSON.parse(localStorage.getItem('newArray'));
 };
 render();
 
     add.addEventListener('click', () => {
         toDoList.push(input.value);
         ul.innerHTML = null;
-        localStorage.setItem('newArray', JSON.stringify(toDoList));
+        localStorage.setItem('list', JSON.stringify(toDoList));
         render();
         input.value = null;
     });
+
+    // localStorage.clear();
+
+const data = () => {
+    if (localStorage.length !== 0 && localStorage.length !== undefined) {
+        toDoList.push(JSON.parse(localStorage.getItem('list')));
+        render();
+    };
+};
+data();
